c9b5d8d0160079c7539d661df7e7818ae6dd2b59
InnaUsenko <innausenko8@gmail.com>
2023-11-07 13:00:40 +0200

    auth

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,14 @@
+//import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+
+export default function Tasks() {
+  return (
+    <>
+      <Helmet>
+        <title>Your contacts</title>
+      </Helmet>
+
+      <h1>HELLO</h1>
+    </>
+  );
+}
